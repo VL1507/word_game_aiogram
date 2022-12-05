3fb82f2c626fe7e53fc9511e795b5fcabf59fa6f
Vladimir Mashyanov <88646034+VL1507@users.noreply.github.com>
2022-12-05 18:51:01 +1200

    Update database.py

--- database.py
+++ database.py
@@ -36,38 +36,28 @@ def create_db() -> None:
         print("<<<<< db создана >>>>>")
 
 
-def insert_update_table(userid: int, username: str, list_of_countries: list, list_of_russian_cities: list) -> None:
+def insert_update_table(msg, list_of_countries: list, list_of_russian_cities: list) -> None:
     """'обнуляет' или создает запись о человеке в countries_game"""
-    
     with sqlite3.connect("my_games.sqlite") as con:
         cur = con.cursor()
 
-        #
-        if (userid,) not in list(cur.execute("SELECT user_id FROM countries_game")):
-
-            cur.execute("INSERT INTO countries_game VALUES (?, ?, ?, ?, ?)",
-                        (userid, username, "_".join(list_of_countries), "_", 'KOSTYL'))
+        if (msg.from_user.id,) not in list(cur.execute("SELECT user_id FROM countries_game")):
+            cur.execute("INSERT INTO countries_game VALUES (?, ?, ?, ?, ?)", (msg.from_user.id, msg.from_user.username, "_".join(list_of_countries), "_", 'KOSTYL'))
         else:
-            cur.execute(
-                "UPDATE countries_game SET bot_lst = ?, used_words = ?, last_letter = ? WHERE user_id  = ?", ("_".join(list_of_countries), '_', 'KOSTYL', userid))
+            cur.execute("UPDATE countries_game SET bot_lst = ?, used_words = ?, last_letter = ? WHERE user_id  = ?", ("_".join(list_of_countries), '_', 'KOSTYL', msg.from_user.id))
 
-        #
-        if (userid,) not in list(cur.execute("SELECT user_id FROM russian_cities_game")):
-
-            cur.execute("INSERT INTO russian_cities_game VALUES (?, ?, ?, ?, ?)",
-                        (userid, username, "_".join(list_of_russian_cities), "_", 'KOSTYL'))
+        
+        if (msg.from_user.id,) not in list(cur.execute("SELECT user_id FROM russian_cities_game")):
+            cur.execute("INSERT INTO russian_cities_game VALUES (?, ?, ?, ?, ?)", (msg.from_user.id, msg.from_user.username, "_".join(list_of_russian_cities), "_", 'KOSTYL'))
         else:
-            cur.execute(
-                "UPDATE russian_cities_game SET bot_lst = ?, used_words = ?, last_letter = ? WHERE user_id  = ?", ("_".join(list_of_russian_cities), '_', 'KOSTYL', userid))
+            cur.execute("UPDATE russian_cities_game SET bot_lst = ?, used_words = ?, last_letter = ? WHERE user_id  = ?", ("_".join(list_of_russian_cities), '_', 'KOSTYL', msg.from_user.id))
 
-        #
-        if (userid,) not in list(cur.execute("SELECT user_id FROM users")):
-
-            cur.execute("INSERT INTO users VALUES (?, ?, ?, ?)",
-                        (userid, username, 0, 0))
+        
+        if (msg.from_user.id,) not in list(cur.execute("SELECT user_id FROM users")):
+            cur.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (msg.from_user.id, msg.from_user.username, 0, 0))
         else:
-            cur.execute(
-                "UPDATE users SET play_countries = ?, play_russian_cities = ? WHERE user_id  = ?", (0, 0, userid))
+            cur.execute("UPDATE users SET play_countries = ?, play_russian_cities = ? WHERE user_id  = ?", (0, 0, msg.from_user.id))
+
 
 
 
