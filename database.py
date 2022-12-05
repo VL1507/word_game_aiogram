ddf87a421a88b7d184e271262a5f58ae1c2c555b
Vladimir Mashyanov <88646034+VL1507@users.noreply.github.com>
2022-12-05 16:32:25 +1200

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import sqlite3
+from random import choice
+
 
 
 def create_db() -> None:
@@ -78,3 +80,55 @@ def insert_update_table(userid: int, username: str, list_of_countries: list, lis
         else:
             cur.execute(
                 "UPDATE users SET play_countries = ?, play_russian_cities = ? WHERE user_id  = ?", (0, 0, userid))
+
+
+
+async def game(bot, msg, table, game_mod):
+    print(msg.text)
+    with sqlite3.connect("my_games.sqlite") as con:
+        cur = con.cursor()
+        last_letter = list(cur.execute(f"SELECT last_letter FROM {table} WHERE user_id = ?", (msg.from_user.id,)))[0][0].upper()
+        print(last_letter)
+        used_words = []
+        for i in cur.execute(f"SELECT used_words FROM {table} WHERE user_id = ?", (msg.from_user.id,)):
+            used_words.append(i[0].replace('_', ' '))
+        used_words = used_words[0].split()
+        if msg.text not in used_words:
+            bot_lst = []
+            for i in cur.execute(f"SELECT bot_lst FROM {table} WHERE user_id = ?", (msg.from_user.id,)):
+                bot_lst.append(i[0])
+            bot_lst = bot_lst[0].split('_')
+            if msg.text in bot_lst:
+                bot_lst.remove(msg.text)
+                used_words.append(msg.text)
+                if msg.text[0] == last_letter or last_letter == "KOSTYL":
+                    print(used_words)
+                    last_letter = msg.text.replace(
+                        'ь', '').replace('ы', '')[-1].upper()
+                    if len([i for i in bot_lst if i[0] == last_letter]) != 0:
+                        bot_worde = choice(
+                            [i for i in bot_lst if i[0] == last_letter])
+                        bot_lst.remove(bot_worde)
+                        used_words.append(bot_worde)
+                        last_letter = bot_worde.replace(
+                            'ь', '').replace('ы', '')[-1].upper()
+                        cur.execute(f"UPDATE {table} SET bot_lst = ?, used_words = ?, last_letter = ? WHERE user_id = ?", ("_".join(
+                            bot_lst), "_".join(used_words), last_letter, msg.from_user.id))
+                        print(used_words)
+                        await bot.send_message(chat_id=msg.from_user.id, text=bot_worde, reply_to_message_id=msg.message_id)
+                        if len([i for i in bot_lst if i[0] == last_letter]) == 0:
+                            cur.execute(
+                                f"UPDATE users SET {game_mod} = ? WHERE user_id = ?", (0, msg.from_user.id))
+                            await msg.answer(f"Бот победил. больше нет слов на <b>{last_letter}</b>")
+                    else:
+                        text = "Поздравляю! Ты победил!"
+                        cur.execute(
+                            f"UPDATE users SET {game_mod} = ? WHERE user_id = ?", (0, msg.from_user.id))
+                        await bot.send_message(chat_id=msg.from_user.id, text=text, reply_to_message_id=msg.message_id)
+                else:
+                    text = f'надо, чтобы слово начиналось на <b>{last_letter}</b>'
+                    await bot.send_message(chat_id=msg.from_user.id, text=text, reply_to_message_id=msg.message_id)
+            else:
+                await msg.answer("я не знаю такое слово :(\nпопробуй ввести другое")
+        else:
+            await msg.answer("такое слово уже было\nпопробуй ввести другое")
