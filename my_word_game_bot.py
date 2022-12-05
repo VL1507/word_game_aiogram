048abb4e1904885c0e649d5591d1900d9906f9d0
Vladimir Mashyanov <88646034+VL1507@users.noreply.github.com>
2022-12-05 16:34:47 +1200

    Update my_word_game_bot.py

--- my_word_game_bot.py
+++ my_word_game_bot.py
@@ -46,25 +46,21 @@ async def russian_cities(msg: types.Message):
 
 @dp.message_handler()
 async def play_game(msg: types.Message):
-    print(msg.from_user.username, 'в play_game')
-
     with sqlite3.connect("my_games.sqlite") as con:
         cur = con.cursor()
         try:
-            play_countries = list(cur.execute(
-                "SELECT play_countries FROM users WHERE user_id = ?", (msg.from_user.id,)))[0][0]
-            play_russian_cities = list(cur.execute(
-                "SELECT play_russian_cities FROM users WHERE user_id = ?", (msg.from_user.id,)))[0][0]
+            play_countries = list(cur.execute("SELECT play_countries FROM users WHERE user_id = ?", (msg.from_user.id,)))[0][0]
+            play_russian_cities = list(cur.execute("SELECT play_russian_cities FROM users WHERE user_id = ?", (msg.from_user.id,)))[0][0]
             
             if play_countries == 1:
                 await game(bot=bot, msg=msg, table='countries_game', game_mod="play_countries")
-
+                
             elif play_russian_cities == 1:
                 await game(bot=bot, msg=msg, table="russian_cities_game", game_mod="play_russian_cities")
-            
+                
             else:
-                print(msg.from_user.username, 'ушел из play_game')
                 await msg.answer("отправьте /countries , чтобы начать игру")
+                
         except IndexError:
             await msg.answer("отправь /help")
 
